4ada5a4bc9fc670e4de2a0e7ac6a59a7bfbe7a60
mk <kamai122000@mail.ru>
2026-06-25 15:42:46 +0300

    pwa

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,6 @@ export default NextAuth(authConfig).auth
 
 export const config = {
 	matcher: [
-		'/((?!_next/static|_next/image|favicon.ico|robots.txt|icons|uploads|api/auth|sw\\.js|manifest\\.webmanifest|icon-192\\.png|icon-512\\.png|apple-touch-icon\\.png).*)',
+		'/((?!_next/static|_next/image|favicon.ico|robots.txt|icons|uploads|screenshots|api/auth|sw\\.js|manifest\\.webmanifest|icon-192\\.png|icon-512\\.png|apple-touch-icon\\.png).*)',
 	],
 }
